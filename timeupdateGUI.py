6bdbe7e4c8efc23c70aae73bc13626c59f1bfa76
Ayo <ayomikuntemitope.araoye@gmail.com>
2023-09-06 15:32:06 +0100

    small-changes

--- timeupdateGUI.py
+++ timeupdateGUI.py
@@ -1,8 +1,10 @@
 import pytz
-from datetime import datetime
+from datetime import datetime, date
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from tkcalendar import Calendar
+from tktimepicker import TimePicker
 
 def convert_timezone(input_datetime, from_timezone, to_timezone):
     # Create timezone objects
@@ -15,134 +17,60 @@ def convert_timezone(input_datetime, from_timezone, to_timezone):
 
     return converted_datetime
 
-def get_selected_datetime():
-    # Get the selected date and time from the calendar and clock
-    selected_date = cal.get_date()
-    selected_time = clock.get()
-    selected_datetime = datetime.combine(selected_date, selected_time)
+def convert_button_clicked():
+    from_timezone = source_timezone_combobox.get()
+    target_timezone = target_timezone_combobox.get()
     
-    # Get the source and target time zones from the dropdown menus
-    from_timezone = from_timezone_var.get()
-    target_timezone = to_timezone_var.get()
+    selected_date = calendar.get_date()
+    selected_time = time_picker.get()
     
-    # Convert the selected datetime to the target timezone
-    converted_datetime = convert_timezone(selected_datetime, from_timezone, target_timezone)
-    
-    # Update the result label
-    result_label.config(text=f"{from_timezone} Time: {selected_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}\n"
-                            f"{target_timezone} Time: {converted_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}")
+    try:
+        year, month, day = map(int, selected_date.split("-"))
+        hour, minute = map(int, selected_time.split(":"))
+        input_datetime = datetime(year, month, day, hour, minute, tzinfo=pytz.utc)
 
-# Create the main application window
-app = tk.Tk()
-app.title("Time Zone Converter")
+        converted_datetime = convert_timezone(input_datetime, from_timezone, target_timezone)
 
-# Create and configure the calendar widget
-cal = Calendar(app, selectmode="day", date_pattern="yyyy-MM-dd")
-cal.pack()
+        result_label.config(text=f"{from_timezone} Time: {input_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}\n"
+                                  f"{target_timezone} Time: {converted_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}")
+    except ValueError:
+        messagebox.showerror("Error", "Invalid date or time format. Please enter a valid date and time.")
 
-# Create and configure the clock widget
-clock = ttk.Combobox(app, values=["00:00:00", "01:00:00", "02:00:00", "03:00:00", "04:00:00",
-                                  "05:00:00", "06:00:00", "07:00:00", "08:00:00", "09:00:00",
-                                  "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00",
-                                  "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00",
-                                  "20:00:00", "21:00:00", "22:00:00", "23:00:00"])
-clock.set("00:00:00")
-clock.pack()
+# Create a Tkinter window
+window = tk.Tk()
+window.title("Time Zone Converter")
 
-# Create dropdown menus for source and target time zones
-available_timezones = pytz.all_timezones
-from_timezone_var = tk.StringVar()
-from_timezone_label = tk.Label(app, text="Select Source Timezone:")
-from_timezone_dropdown = ttk.Combobox(app, textvariable=from_timezone_var, values=available_timezones)
-from_timezone_label.pack()
-from_timezone_dropdown.pack()
+# Create and populate the source timezone combobox
+source_timezone_label = ttk.Label(window, text="Source Timezone:")
+source_timezone_label.pack()
+source_timezone_combobox = ttk.Combobox(window, values=pytz.all_timezones)
+source_timezone_combobox.pack()
 
-to_timezone_var = tk.StringVar()
-to_timezone_label = tk.Label(app, text="Select Target Timezone:")
-to_timezone_dropdown = ttk.Combobox(app, textvariable=to_timezone_var, values=available_timezones)
-to_timezone_label.pack()
-to_timezone_dropdown.pack()
+# Create and populate the target timezone combobox
+target_timezone_label = ttk.Label(window, text="Target Timezone:")
+target_timezone_label.pack()
+target_timezone_combobox = ttk.Combobox(window, values=pytz.all_timezones)
+target_timezone_combobox.pack()
 
-# Create a button to perform the conversion
-convert_button = tk.Button(app, text="Convert", command=get_selected_datetime)
+# Create a calendar widget
+calendar_label = ttk.Label(window, text="Select Date:")
+calendar_label.pack()
+calendar = Calendar(window)
+calendar.pack()
+
+# Create a time picker widget
+time_label = ttk.Label(window, text="Select Time:")
+time_label.pack()
+time_picker = TimePicker(window)
+time_picker.pack()
+
+# Create a button to trigger the conversion
+convert_button = ttk.Button(window, text="Convert", command=convert_button_clicked)
 convert_button.pack()
 
 # Create a label to display the result
-result_label = tk.Label(app, text="", justify="left")
+result_label = ttk.Label(window, text="")
 result_label.pack()
 
-app.mainloop()
-import pytz
-from datetime import datetime
-import tkinter as tk
-from tkinter import ttk
-from tkcalendar import Calendar
-
-def convert_timezone(input_datetime, from_timezone, to_timezone):
-    # Create timezone objects
-    from_tz = pytz.timezone(from_timezone)
-    to_tz = pytz.timezone(to_timezone)
-
-    # Convert the input datetime from the source timezone to the target timezone
-    input_datetime = from_tz.localize(input_datetime)
-    converted_datetime = input_datetime.astimezone(to_tz)
-
-    return converted_datetime
-
-def get_selected_datetime():
-    # Get the selected date and time from the calendar and clock
-    selected_date = cal.get_date()
-    selected_time = clock.get()
-    selected_datetime = datetime.combine(selected_date, selected_time)
-    
-    # Get the source and target time zones from the dropdown menus
-    from_timezone = from_timezone_var.get()
-    target_timezone = to_timezone_var.get()
-    
-    # Convert the selected datetime to the target timezone
-    converted_datetime = convert_timezone(selected_datetime, from_timezone, target_timezone)
-    
-    # Update the result label
-    result_label.config(text=f"{from_timezone} Time: {selected_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}\n"
-                            f"{target_timezone} Time: {converted_datetime.strftime('%Y-%m-%d %H:%M:%S %Z')}")
-
-# Create the main application window
-app = tk.Tk()
-app.title("Time Zone Converter")
-
-# Create and configure the calendar widget
-cal = Calendar(app, selectmode="day", date_pattern="yyyy-MM-dd")
-cal.pack()
-
-# Create and configure the clock widget
-clock = ttk.Combobox(app, values=["00:00:00", "01:00:00", "02:00:00", "03:00:00", "04:00:00",
-                                  "05:00:00", "06:00:00", "07:00:00", "08:00:00", "09:00:00",
-                                  "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00",
-                                  "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00",
-                                  "20:00:00", "21:00:00", "22:00:00", "23:00:00"])
-clock.set("00:00:00")
-clock.pack()
-
-# Create dropdown menus for source and target time zones
-available_timezones = pytz.all_timezones
-from_timezone_var = tk.StringVar()
-from_timezone_label = tk.Label(app, text="Select Source Timezone:")
-from_timezone_dropdown = ttk.Combobox(app, textvariable=from_timezone_var, values=available_timezones)
-from_timezone_label.pack()
-from_timezone_dropdown.pack()
-
-to_timezone_var = tk.StringVar()
-to_timezone_label = tk.Label(app, text="Select Target Timezone:")
-to_timezone_dropdown = ttk.Combobox(app, textvariable=to_timezone_var, values=available_timezones)
-to_timezone_label.pack()
-to_timezone_dropdown.pack()
-
-# Create a button to perform the conversion
-convert_button = tk.Button(app, text="Convert", command=get_selected_datetime)
-convert_button.pack()
-
-# Create a label to display the result
-result_label = tk.Label(app, text="", justify="left")
-result_label.pack()
-
-app.mainloop()
+# Start the Tkinter main loop
+window.mainloop()
